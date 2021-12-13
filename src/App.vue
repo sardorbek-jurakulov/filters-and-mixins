a8d3a1affc69f4b48b73712dc6ef02876d1feb62
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2021-12-13 12:40:42 +0500

    WIP

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,26 @@
                 <!-- Build a local Filter which reverses the Text it is applied on -->
                 <h3>Name reverser</h3>
                 <input type="text" name="userName" v-model.lazy="userName" placeholder="Your name here">
-                Reversed version of your Name is: {{ userName | nameReverser }}
+                
+                <!-- Reversed version of your Name is: {{ userName | nameReverser }} -->
                 
 
-                <br><hr><br>
+                <!-- <br><hr><br> -->
                 <!-- Exercise 2 -->
                 <!-- Build a global Filter which counts the length of a word and it appends it -->
                 <!-- Like this: "Test" => Gets Filtered to => "Test (4)" -->
-                Your Name info: {{ userName | stringLengthCounter }}
+                
+                <!-- Your Name info: {{ userName | stringLengthCounter }} -->
+
+
+                <br><hr><br>
                 <!-- Exercise 3 -->
                 <!-- Do the same as in Exercises 1 & 2, now with Computed Properties -->
+                <div v-if="userName != ''">
+                  Reversed version of your Name is: {{ nameReverserComputed }}
+                  <br><hr><br>
+                  Your Name info: {{ stringLengthCounterComputed }}
+                </div>
 
                 <!-- Exercise 4 -->
                 <!-- Share the Computed Property rebuilding Exercise 2 via a Mixin -->
@@ -35,6 +45,14 @@
       filters: {
         nameReverser(value) {
           return  value.split("").reverse().join("");
+        }
+      },
+      computed: {
+        nameReverserComputed() {
+          return this.userName.split("").reverse().join("");
+        },
+        stringLengthCounterComputed() {
+          return this.userName + " " + "(" + this.userName.length + ")";
         }
       }
     }
